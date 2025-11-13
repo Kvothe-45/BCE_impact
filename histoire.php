80de9c6e16b8aa0d1bec29fdc84f25152bd8990c
raphoulefoufou <pastagraphael@gmail.com>
2025-11-13 18:19:38 +0100

    Add files via upload

---
+++ histoire.php
@@ -0,0 +1,71 @@
+<DOCTYPE html>
+
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>BCE IMPACT</title>
+    <link rel="stylesheet" href="styles/style_histoire.css">
+</head>
+    
+<body>
+    <?php include 'header.php'?>
+    <h2>Naissance de l'UE et de la BCE</h2>
+    <div class="boite_histoire">
+        <p>Fondée en 1951, la Communauté européenne du charbon et de l’acier  constitue le premier pas vers une paix durable. En 1957, le traité de  Rome établit la Communauté économique européenne (CEE) et ouvre une  nouvelle ère de coopération toujours plus étroite en Europe. Toutefois,  cette période marque également le début d’une guerre froide qui divise  le continent pendant plus de 40 ans.</p></br>
+        <p> <span class="SGM"  >8 mai 1945 – Fin de la Seconde Guerre mondiale en Europe</br>
+La Seconde Guerre mondiale s’achève en Europe. Le  continent est dévasté.  </br> </br> </p>
+<p>4 avril 1949 – Création de l’OTAN:
+L’Organisation du traité de l’Atlantique Nord (OTAN)</br>
+ est créée. Il s’agit d’une alliance intergouvernementale pour la  sécurité entre les États-Unis, le Canada et 10 pays d’Europe  occidentale. En 2020, l’OTAN
+ compte 30 membres, dont 21 pays de l’UE.</p>
+ <p>5 mai 1949 – Établissement du Conseil de l’Europe
+10 pays d’Europe occidentale
+ établissent le Conseil de l’Europe, afin de promouvoir la démocratie et protéger les droits de l’homme et l’état de droit. La Convention européenne des droits de l’homme
+ entre en vigueur le 3 septembre 1953.</p></br>
+ <p>9 mai 1950 – Un plan pour une nouvelle coopération politique en Europe
+Robert Schuman, ministre français des affaires étrangères, présente un plan pour une coopération plus poussée. Il propose d’intégrer les industries du charbon et de l’acier des pays d’Europe occidentale. Le 9 mai sera  plus tard célébré par l’Union européenne comme la «Journée de l’Europe».</p></br>
+<p>18 avril 1951 – Communauté européenne du charbon et de l’acier
+Sur la base du plan Schuman, six pays signent un traité pour mettre en commun la gestion de leurs industries du charbon et de  l’acier. De cette manière, aucun d'entre eux ne peut fabriquer seul des  armes de guerre pour attaquer les autres, comme c'était le cas dans le  passé. Ces six pays sont l’Allemagne, la France, l’Italie, les Pays-Bas, la Belgique et le Luxembourg. La Communauté européenne du charbon et de l’acier voit le jour en 1952.</p>  </br>  
+<p>25 mars 1957 – Traités de Rome
+S’appuyant sur le succès du traité du charbon et de  l’acier, les 6 pays fondateurs étendent leur coopération à d’autres  secteurs économiques. Ils formalisent cette coopération par la signature de deux traités, instituant la Communauté économique européenne (CEE) et la Communauté  européenne de l’énergie atomique (Euratom). Ces organes voient le jour  le 1er janvier 1958.</p></br>
+<p>19 mars 1958 – Naissance du Parlement européen
+La première session de l’Assemblée parlementaire  européenne, précurseur du Parlement européen d’aujourd’hui, se tient à  Strasbourg (France) et Robert Schuman en devient le président (Ursula von der Leyen préside la Commission européenne depuis 2019). Ces sessions  remplacent l’Assemblée commune de la Communauté européenne du charbon et  de l’acier et prend le nom de Parlement européen le 30 mars 1962.</p></br>
+<p>L’Union économique et monétaire </br>
+
+En juin 1988, le Conseil européen a confirmé l’objectif  de l’instauration, par étapes, d’une Union économique et monétaire  (UEM).</br>
+
+ Il a chargé un comité placé sous la présidence de Jacques Delors, président de la Commission européenne, d’étudier et de proposer les  étapes concrètes devant mener à cette union.</br>
+
+Ce comité comprenait les gouverneurs des banques  centrales nationales de la Communauté européenne ainsi que MM. Alexandre Lamfalussy, alors Directeur général de la Banque des règlements  internationaux, Niels Thygesen, professeur d’économie au Danemark, et  Miguel Boyer, alors président du Banco Exterior de España.</br>
+
+Le Rapport Delors rédigé par ce comité proposait trois étapes successives pour réaliser l’UEM expliqué sur le site : 
+https://www.ecb.europa.eu/ecb/history-arts-culture/history/emu/html/index.fr.html</p>
+
+
+
+<img class="Guerre" src="Images_histoire/guerrefinie.jpg" alt= "La guerre est "/>
+<img class="Assemblée" src="Images_histoire/hommes.jpg" alt="Assemblée "/>
+<img class="Carte" src="Images_histoire/carte.jpg" alt="Carte des pays fondateurs de l'UE" title="Carte des pays fondateurs de l'UE"/>
+<img class="OTAN" src="Images_histoire/NATO.jpg" alt="Logo de l'OTAN" title="OTAN"/>
+<img class="Schuman" src="Images_histoire/CHIRAC.jpg" alt="Robert Schuman" title="Robert Schuman"/>
+<img class="Parlement" src="Images_histoire/parlement.jpg" alt="Parlement européen" title="Parlement européen"/>
+
+</div>
+<img class="billet" src="Images_histoire/50euros.jpg" alt="Billet de 50 euros" title="Billet de 50 euros"/>
+
+</body>
+
+<footer>
+    <a href="Home.html">
+        <img src="logo.png" alt="Logo de BCE Impact">
+    </a>
+    <h4>Projet L3 MIASHS</h4>
+    <ul>
+        <li><a>Pour aller plus loin</a></li>
+        <li><a>Commentaires</a></li>
+        <li><a>Sources</a></li>
+        <li><a>Contact</a></li>
+    </ul>
+</footer>
+
+</html>
