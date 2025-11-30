4c9a2826afb2837d7d29359d9842b660ff087201
raphoulefoufou <pastagraphael@gmail.com>
2025-11-30 16:46:29 +0100

    Add files via upload

--- sources.php
+++ sources.php
@@ -1,46 +1,91 @@
-
 <!DOCTYPE html>
-
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>BCE IMPACT</title>
+    <title>BCE IMPACT - Sources</title>
     <link rel="stylesheet" href="styles/style_source.css">
 </head>
 
 <body>
-    <?php include 'header.php'; ?> 
 
-    <h2 id="source">Sources</h2>
-    <a class="Notre"> Notre base de données tient ses tables des sources que voici :</a>
-    <a class="Insee" href="https://www.insee.fr/fr/accueil"> Insee</a>
-    <a class="BCE" href="https://www.ecb.europa.eu/home/html/index.fr.html"> BCE</a>
-    <a class="UE" href="https://european-union.europa.eu/index_fr"> Union Europeene </a>
-    <h3 id="papl">Pour aller plus loin</h3>
-    <p class="plus">Vous voudriez vous renseigner plus profondément sur l’économie ou la </br>politique ?  Voici des sources qui pourront vous intéresser :</p>
-    <a class="CE" href="https://commission.europa.eu/index_fr"> Comission Europeene </a>
-    <a class="UEBF" href="https://www.banque-france.fr/fr"> Banque de France </a>
-    <a class="GF" href="https://www.info.gouv.fr/">Gouvernement Francais </a>
-    <p class="vous">Vous voudriez vous renseigner plus profondément sur le raisonnement </br> humain ? Voici des sources qui pourront vous intéresser :</p>
-    <a class="ES" href="https://www.youtube.com/@JLMelenchon">L'esprit critique</a>
-    <a class="ETSF" href="https://www.youtube.com/@Ettoutlemondesenfout">Et tout le monde s'en fout </a>
-    <p class="reportage">Vous appréciez les reportages entrainant ? Voici des sources qui pourront </br> vous intéresser :</p>
-    <a class="ARTE" href="https://www.arte.tv/fr/">ARTE</a>
-    <a class="Minute" href="https://www.arte.tv/fr/videos/RC-014077/28-minutes/">28 Minutes</a>
-<div id="footer">
-    <footer>
+    <?php include 'header.php'; ?>
+
+    <main class="container">
+
+        <h2>Sources</h2>
+        <p class="intro">Notre base de données tient ses tables des sources suivantes :</p>
+
+        <ul class="list">
+            <li><a href="https://www.insee.fr/fr/accueil">INSEE</a></li>
+            <li><a href="https://www.ecb.europa.eu/home/html/index.fr.html">BCE</a></li>
+            <li><a href="https://european-union.europa.eu/index_fr">Union Européenne</a></li>
+        </ul>
+
+        <h3>Pour aller plus loin</h3>
+        <p>Vous voudriez vous renseigner plus profondément sur l’économie, l'histoire ou la politique ? Voici des sources qui pourront vous intéresser :</p>
+
+        <ul class="list">
+            <li><a href="https://commission.europa.eu/index_fr">Commission Européenne</a></li>
+            <li><a href="https://www.banque-france.fr/fr">Banque de France</a></li>
+            <li><a href="https://www.info.gouv.fr/">Gouvernement Français</a></li>
+            <li><a href="https://www.touteleurope.eu/" >touteleurope</a><li>
+            <li><a href="https://www.toutelhistoire.com/">TouteL'Histoire</a><li>
+                <li><a href="https://www.france.tv/documentaires/documentaires-histoire/">France TV</a><li> 
+
+        </ul>
+
+        <p>Vous voudriez vous renseigner plus profondément sur le raisonnement humain ? Voici des sources qui pourront vous intéresser :</p>
+
+        <ul class="list">
+            <li><a href="https://www.youtube.com/@lespritcritique">L'Esprit Critique</a></li>
+            <li><a href="https://www.youtube.com/@Ettoutlemondesenfout">Et tout le monde s'en fout</a></li>
+        </ul>
+
+        <p>Vous appréciez les reportages entraînants ? Voici des sources qui pourront vous intéresser :</p>
+
+        <ul class="list">
+            <li><a href="https://www.arte.tv/fr/">ARTE</a></li>
+            <li><a href="https://www.arte.tv/fr/videos/RC-014077/28-minutes/">28 Minutes</a></li>
+        </ul>
+        <p>Voici la principale source utilisée pour chaque pays :</p>
+        <ul class="list">
+<li><a href="">Allemagne</a></li>
+<li><a href="">Autriche</a></li>
+<li><a href="">Belgique</a></li>
+<li><a href="">Chypre  </a></li>
+<li><a href="">Espagne</a></li>
+<li><a href="https://www.cavalngo.com/guide-voyage/infos-pratiques-pays/details/24/134-histoire-estonie.html">Estonie</a></li>
+<li><a href="">France</a></li>
+<li><a href="">Grèce</a></li>
+<li><a href="">Irlande </a></li>
+<li><a href="">Italie </a></li>
+<li><a href="https://www.cavalngo.com/guide-voyage/infos-pratiques-pays/details/24/135-histoire-lettonie.html">Lettonie </a></li>
+<li><a href="">Lituanie</a></li>
+<li><a href="">Luxembourg </a></li>
+<li><a href="">Malte</a></li>
+<li><a href="">Pays-Bas </a></li>
+<li><a href="">Portugal </a></li>
+<li><a href="https://sk.ambafrance.org/La-Slovaquie-en-quelques-dates">Slovaquie</a></li>
+<li><a href="">Slovénie</a></li>
+<li><a href="">Croatie </a></li>
+        </ul>
+
+    </main>
+
+    <footer id="footer">
         <a href="Home.php">
             <img src="images/logo.png" alt="Logo de BCE Impact">
         </a>
+
         <h2>Projet L3 MIASHS</h2>
+
         <ul>
-            <li><a href="sources.php">Pour aller plus loin</a></li>
-            <li><a href="commentaires.php">Commentaires</a></li>
             <li><a href="sources.php">Sources</a></li>
+            <li><a href="commentaires.php">Commentaires</a></li>
+            <li><a href="plus.php">Pour aller plus loin</a></li>
             <li><a href="quisommesnous.php">Contact</a></li>
         </ul>
     </footer>
-</div>
-</body>
 
+</body>
 </html>
