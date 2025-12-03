275ddd9d3dd975de532f0445ac1a5daff4616125
Sidney Dachez <sidsmog@gmail.com>
2025-12-03 22:39:47 +0100

    modif histoire

--- histoire.php
+++ histoire.php
@@ -10,37 +10,43 @@
 <body>
     <?php include 'header.php'?>
     <h2 id="h2his">Naissance de l'UE et de la BCE</h2>
-    <div class="boite_histoire">
-        <p>Fondée en 1951, la Communauté européenne du charbon et de l’acier  constitue le premier pas vers une paix durable. En 1957, le traité de  Rome établit la Communauté économique européenne (CEE) et ouvre une  nouvelle ère de coopération toujours plus étroite en Europe. Toutefois,  cette période marque également le début d’une guerre froide qui divise  le continent pendant plus de 40 ans.</p></br>
-        <p> <span class="SGM"  >8 mai 1945 – Fin de la Seconde Guerre mondiale en Europe</br>
-La Seconde Guerre mondiale s’achève en Europe. Le  continent est dévasté.  </br> </br> </p>
-<p>4 avril 1949 – Création de l’OTAN:
-L’Organisation du traité de l’Atlantique Nord (OTAN)</br>
+    <div id="boite_histoire">
+<p id="init_text">Fondée en 1951, la Communauté européenne du charbon et de l’acier  constitue le premier pas vers une paix durable. En 1957, le traité de  Rome établit la Communauté économique européenne (CEE) et ouvre une  nouvelle ère de coopération toujours plus étroite en Europe. Toutefois,  cette période marque également le début d’une guerre froide qui divise  le continent pendant plus de 40 ans.</p>
+<h4 class="title_para">8 mai 1945 – Fin de la Seconde Guerre mondiale en Europe</h4>
+<p>La Seconde Guerre mondiale s’achève en Europe. Le  continent est dévasté.</p>
+<h4 class="title_para">4 avril 1949 – Création de l’OTAN</h4>
+<p>L’Organisation du traité de l’Atlantique Nord (OTAN)
  est créée. Il s’agit d’une alliance intergouvernementale pour la  sécurité entre les États-Unis, le Canada et 10 pays d’Europe  occidentale. En 2020, l’OTAN
  compte 30 membres, dont 21 pays de l’UE.</p>
- <p>5 mai 1949 – Établissement du Conseil de l’Europe
-10 pays d’Europe occidentale
+<h4 class="title_para">5 mai 1949 – Création d'un conseil d'Europe</h4>
+<p>10 pays d’Europe occidentale
  établissent le Conseil de l’Europe, afin de promouvoir la démocratie et protéger les droits de l’homme et l’état de droit. La Convention européenne des droits de l’homme
- entre en vigueur le 3 septembre 1953.</p></br>
- <p>9 mai 1950 – Un plan pour une nouvelle coopération politique en Europe
-Robert Schuman, ministre français des affaires étrangères, présente un plan pour une coopération plus poussée. Il propose d’intégrer les industries du charbon et de l’acier des pays d’Europe occidentale. Le 9 mai sera  plus tard célébré par l’Union européenne comme la «Journée de l’Europe».</p></br>
-<p>18 avril 1951 – Communauté européenne du charbon et de l’acier
-Sur la base du plan Schuman, six pays signent un traité pour mettre en commun la gestion de leurs industries du charbon et de  l’acier. De cette manière, aucun d'entre eux ne peut fabriquer seul des  armes de guerre pour attaquer les autres, comme c'était le cas dans le  passé. Ces six pays sont l’Allemagne, la France, l’Italie, les Pays-Bas, la Belgique et le Luxembourg. La Communauté européenne du charbon et de l’acier voit le jour en 1952.</p>  </br>  
-<p>25 mars 1957 – Traités de Rome
-S’appuyant sur le succès du traité du charbon et de  l’acier, les 6 pays fondateurs étendent leur coopération à d’autres  secteurs économiques. Ils formalisent cette coopération par la signature de deux traités, instituant la Communauté économique européenne (CEE) et la Communauté  européenne de l’énergie atomique (Euratom). Ces organes voient le jour  le 1er janvier 1958.</p></br>
-<p>19 mars 1958 – Naissance du Parlement européen
-La première session de l’Assemblée parlementaire  européenne, précurseur du Parlement européen d’aujourd’hui, se tient à  Strasbourg (France) et Robert Schuman en devient le président (Ursula von der Leyen préside la Commission européenne depuis 2019). Ces sessions  remplacent l’Assemblée commune de la Communauté européenne du charbon et  de l’acier et prend le nom de Parlement européen le 30 mars 1962.</p></br>
-<p>L’Union économique et monétaire </br>
-
-En juin 1988, le Conseil européen a confirmé l’objectif  de l’instauration, par étapes, d’une Union économique et monétaire  (UEM).</br>
-
- Il a chargé un comité placé sous la présidence de Jacques Delors, président de la Commission européenne, d’étudier et de proposer les  étapes concrètes devant mener à cette union.</br>
-
-Ce comité comprenait les gouverneurs des banques  centrales nationales de la Communauté européenne ainsi que MM. Alexandre Lamfalussy, alors Directeur général de la Banque des règlements  internationaux, Niels Thygesen, professeur d’économie au Danemark, et  Miguel Boyer, alors président du Banco Exterior de España.</br>
-
-Le Rapport Delors rédigé par ce comité proposait trois étapes successives pour réaliser l’UEM expliqué sur le site : 
-https://www.ecb.europa.eu/ecb/history-arts-culture/history/emu/html/index.fr.html</p>
-
+ entre en vigueur le 3 septembre 1953.</p>
+<h4 class="title_para">9 mai 1950 – Un plan pour une nouvelle coopération politique en Europe</h4>
+<p>Robert Schuman, ministre français des affaires étrangères, présente un plan pour une coopération plus poussée. Il propose d’intégrer les industries du charbon et de l’acier des pays d’Europe occidentale. Le 9 mai sera  plus tard célébré par l’Union européenne comme la «Journée de l’Europe».</p>
+<h4 class="title_para">18 avril 1951 – Communauté européenne du charbon et de l’acier</h4>
+<p>Sur la base du plan Schuman, six pays signent un traité pour mettre en commun la gestion de leurs industries du charbon et de  l’acier. De cette manière, aucun d'entre eux ne peut fabriquer seul des  armes de guerre pour attaquer les autres, comme c'était le cas dans le  passé. Ces six pays sont l’Allemagne, la France, l’Italie, les Pays-Bas, la Belgique et le Luxembourg. La Communauté européenne du charbon et de l’acier voit le jour en 1952.</p>
+<h4 class="title_para">25 mars 1957 – Traités de Rome</h4>
+<p>S’appuyant sur le succès du traité du charbon et de  l’acier, les 6 pays fondateurs étendent leur coopération à d’autres  secteurs économiques. Ils formalisent cette coopération par la signature de deux traités, instituant la Communauté économique européenne (CEE) et la Communauté  européenne de l’énergie atomique (Euratom). Ces organes voient le jour  le 1er janvier 1958.</p>
+<h4 class="title_para">19 mars 1958 – Naissance du Parlement européen</h4>
+<p>La première session de l’Assemblée parlementaire  européenne, précurseur du Parlement européen d’aujourd’hui, se tient à  Strasbourg (France) et Robert Schuman en devient le président (Ursula von der Leyen préside la Commission européenne depuis 2019). Ces sessions  remplacent l’Assemblée commune de la Communauté européenne du charbon et  de l’acier et prend le nom de Parlement européen le 30 mars 1962.</p>
+<h4 class="title_para">7 février 1992 – Traité de Maastricht</h4>
+<p>Le traité sur l'Union européenne est signé à Maastricht, aux Pays-Bas. Il représente une avancée majeure, car il établit des règles claires pour la future monnaie unique, la politique étrangère et de sécurité et une coopération plus étroite dans les domaines de la justice et des affaires intérieures. L’«Union européenne» est officiellement créée par le traité, qui entre en vigueur le 1er novembre 1993.</p>
+<p id="last_para">
+En juin 1988, le Conseil européen a confirmé l’objectif  de l’instauration, par étapes, d’une Union économique et monétaire (UEM).</br>
+Il a chargé un comité placé sous la présidence de Jacques Delors, président de la Commission européenne, d’étudier et de proposer les  étapes concrètes devant mener à cette union.</br>
+Ce comité comprenait les gouverneurs des banques  centrales nationales de la Communauté européenne ainsi que MM. Alexandre Lamfalussy, alors Directeur général de la Banque des règlements  internationaux, Niels Thygesen, professeur d’économie au Danemark, et  Miguel Boyer, alors président du Banco Exterior de España.
+Le Rapport Delors rédigé par ce comité proposait trois phase successives pour réaliser l’UEM :
+</p>
+<ul id="liste_phase">
+    <li>-Le 1er juillet 1990 à eu lieu le commencement de l'abolition de toutes les restrictions aux mouvements de capitaux entre les États membres. Par la suite des préparatifs notamment juridiques se réaliseront</li>
+    <li>-Création de l’IME (Institut monétaire européen) et de la BCE qui mènera notamment à la création de nouveaux billets en décembre 1996 ou du Pacte de stabilité et de croissance. Des préparatifs pour la fixation des taux de conversion ainsi que les nominations au sein des organes de décision se feront en mai 1998</li>
+    <li>-Le 1er janvier 1999, la fixation irrévocable des taux de conversion des monnaies des onze États membres participant dès le début a été appliqué sous une politique monétaire unique sous la responsabilité de la BCE. L’euro est né</li>
+</ul>
+<p>Pour plus d'information cliquer <a href="https://www.ecb.europa.eu/ecb/history-arts-culture/history/emu/html/index.fr.html">ICI</a>
+</p>
+<p id="last_comment">Aujourd'hui, l'Union européenne compte 27 pays membres mais sept d’entre eux n’utilisent pas l’euro comme monnaie courante.</br> En effet, une adhésion à l’Union européenne n’induit pas nécessairement l’adoption de la monnaie unique ! </br></br>Pour adopter la monnaie unique, la Commission européenne et la Banque centrale européenne évaluent ensemble si les pays candidats à l’euro remplissent les conditions nécessaires à l’adoption de la monnaie unique.</br> Après examen des progrès réalisés selon les critères de convergence, elles présentent leurs conclusions dans des rapports distincts. </br>Ces rapports sont ensuite approuvés par le Conseil ECOFIN, en concertation avec le Parlement européen et les chefs d’État. </br>En cas d’avis favorable, le processus d’intégration à la zone euro peut alors être engagé. 
+</p>
 
 
 <img class="Guerre" src="Images_histoire/guerrefinie.jpg" alt= "La guerre est "/>
